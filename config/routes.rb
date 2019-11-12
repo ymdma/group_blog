6f81b56c0feb0b4501bfb75fb330c6de2944b671
ymdma <m.yamada1985@gmail.com>
2019-11-12 21:26:42 +0900

    data-disp-test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # get 'users/create'
+  # get 'posts/index'
   devise_for :users
+  root "posts#index"
+
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
