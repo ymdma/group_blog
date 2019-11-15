a8c478c248da0432c53ad31f2eeaf54785dc838c
ymdma <m.yamada1985@gmail.com>
2019-11-15 21:37:53 +0900

    add_foreign_key_true

--- db/migrate/20191115120906_add_column_group_id.rb
+++ db/migrate/20191115120906_add_column_group_id.rb
@@ -1,5 +1,5 @@
 class AddColumnGroupId < ActiveRecord::Migration[5.2]
   def change
-    add_reference :posts, :group
+    add_reference :posts, :group, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_120403) do
+ActiveRecord::Schema.define(version: 2019_11_15_120906) do
 
   create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "group_id"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2019_11_15_120403) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id"
+    t.index ["group_id"], name: "index_posts_on_group_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -53,5 +55,6 @@ ActiveRecord::Schema.define(version: 2019_11_15_120403) do
 
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
+  add_foreign_key "posts", "groups"
   add_foreign_key "posts", "users"
 end
