ac18742f69a2c13a8418f75646ddf43b03bcbafe
ymdma <m.yamada1985@gmail.com>
2019-11-15 14:48:46 +0900

    TAB
    各フォームやコンテンツを展開するタブ

---
+++ app/assets/javascripts/tab.js
@@ -0,0 +1,20 @@
+
+$(function(){
+
+  $(".form_signin_tab").click(function(){
+    $(".form_signin_two").toggleClass("hidden");
+  });
+
+  $(".form_signup_tab").click(function(){
+    $(".form_signup_two").toggleClass("hidden");
+  });
+
+  $(".form_post_tab").click(function(){
+    $(".form_post_two").toggleClass("hidden");
+  });
+
+  $(".contents_tab").click(function(){
+    $(".contents_two").toggleClass("hidden");
+  });
+
+});
