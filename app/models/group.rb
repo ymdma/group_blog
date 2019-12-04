227f5290ae3d085d597e81174d5b58474a8f9fb8
ymdma <m.yamada1985@gmail.com>
2019-12-04 13:55:09 +0900

    valid-off

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,11 +4,11 @@ class Group < ApplicationRecord
   has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users
 
-  validates :group_name, numericality: { only_interger: false }
-  validates :group_name, presence: true
-  validates :group_name, uniqueness: { case_sensitive: :true}
-  validates :group_name, length: { minimum: 1, maximum: 10 }
-  validates :group_name, confirmation: { case_sensitive: true }
+  # validates :group_name, numericality: { only_interger: false }
+  # validates :group_name, presence: true
+  # validates :group_name, uniqueness: { case_sensitive: :true}
+  # validates :group_name, length: { minimum: 1, maximum: 10 }
+  # validates :group_name, confirmation: { case_sensitive: true }
 
 
 
