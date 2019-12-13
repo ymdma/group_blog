a638fd980b1bca9f382a023507efe14eb3c59cee
ymdma <m.yamada1985@gmail.com>
2019-12-14 07:52:02 +0900

    User_Profile
    addColumn_Profile_for_Users
    and_Edit-PopUp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_120906) do
+ActiveRecord::Schema.define(version: 2019_12_13_210338) do
 
   create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "group_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_11_15_120906) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
